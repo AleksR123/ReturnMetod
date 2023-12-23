d809c3894dc67c662be40c803921f44f637b80b7
unknown <aleksandarristov28@gmail.com>
2023-12-23 18:09:33 +0100

    first java program

---
+++ src/ReturnMetod.java
@@ -0,0 +1,15 @@
+
+public class ReturnMetod {
+	
+		public static void main (String[] args) {
+			
+			int x = 10;
+			int y= myFunc(x);
+			System.out.println(y);
+			
+		}
+		public static int myFunc(int x) {
+			return x*3;
+		}
+
+}
